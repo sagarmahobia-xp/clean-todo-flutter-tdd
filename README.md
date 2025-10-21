0a3dfdd8d021b7526417d7ae33edcb061de2bca3
sagarmahobia <sagar@incubyte.co>
2025-10-21 14:25:55 +0530

    Initial commit. Project setup.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Clean Todo TDD
+
+A todo add app using Drift DB, following the clean architecture and test-driven development principles.
+ 
---
+++ android/app/src/main/kotlin/com/incubyte/tdd/clean/todo/clean_todo_tdd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.incubyte.tdd.clean.todo.clean_todo_tdd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
